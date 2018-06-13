2774670d67ae21fe8923aff6e0928dfaa6c3318a
Owashi Shoichi <owashi@openwave.co.jp>
2018-06-13 16:22:28 +0900

    DEBUG OFF

--- src/lmic/config.h
+++ src/lmic/config.h
@@ -84,7 +84,7 @@
 // configured (e.g. on AVR it is not by default), otherwise using it can
 // cause crashing.
 #ifndef LMIC_DEBUG_LEVEL
-#define LMIC_DEBUG_LEVEL 2
+#define LMIC_DEBUG_LEVEL 0
 #endif
 
 // Enable this to allow using printf() to print to the given serial port
